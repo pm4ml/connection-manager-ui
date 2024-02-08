bc02cfbbbc9a8b0f0adf958b2af147d5193707d1
Kalin Krustev <kalin.krustev@gmail.com>
2024-02-08 10:54:04 +0000

    chore: update nix

--- integration_test/default.nix
+++ integration_test/default.nix
@@ -1,7 +1,7 @@
 # Want to update dependencies? Go to https://github.com/NixOS/nixpkgs/tree/nixpkgs-unstable and
 # replace the commit ref in the following line with the one you're interested in (probably the
 # latest).
-# { nixpkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/08ef0f28e3a41424b92ba1d203de64257a9fca6a.tar.gz) { config = { allowUnfree = true; }; } }:
+{ nixpkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz) { config = { allowUnfree = true; }; } }:
 
 let
   k3d = nixpkgs.stdenv.mkDerivation rec {
