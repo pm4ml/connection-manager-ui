737efd6e2cdf39f7eef0a0db673a1051c26330b9
Bupe <ngasebupe@gmail.com>
2025-02-07 10:23:33 +0200

    reset workflow errors

--- integration_test/default.nix
+++ integration_test/default.nix
@@ -10,7 +10,7 @@ let
 
     src = builtins.fetchurl {
       url = "https://github.com/rancher/k3d/releases/download/v5.4.6/k3d-linux-amd64";  # Updated URL
-      sha256 = "0v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z";  # Updated SHA256
+      sha256 = "0v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z";  
     };
 
     dontUnpack = true;
@@ -29,7 +29,7 @@ let
     pname = "skaffold";
     src = builtins.fetchurl {
       url = "https://github.com/GoogleContainerTools/skaffold/releases/download/v1.35.0/skaffold-linux-amd64";  # Updated URL
-      sha256 = "0v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z";  # Updated SHA256
+      sha256 = "0v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z"; 
     };
     dontUnpack = true;
     installPhase = ''
